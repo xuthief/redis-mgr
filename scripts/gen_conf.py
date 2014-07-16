315b5a2bb43dba3f1fb1fc5e6a9a28eb47b80ed2
idning <idning@gmail.com>
2014-07-16 16:52:04 +0800

    new gen_conf.py

--- scripts/gen_conf.py
+++ scripts/gen_conf.py
@@ -13,38 +13,78 @@ from pcl import common
 from string import Template as T
 T.s = T.substitute
 
+USER = 'ning'
+
+cluster_id = 0
 CLUSTER_NAME = 'cluster0'
-BASEDIR = '/tmp/r'
-HOSTS = [
-        '127.0.0.5',
-        '127.0.0.5',
-        ]
+BASEDIR = '/home/ning/redis-cluster0'
+
 MASTER_PER_MACHINE = 2
+HOSTS = [
+    '127.0.0.5',
+    '127.0.0.5',
+]
 
-# gen the "redis" section
-port = 2000
-for i in range(len(HOSTS)):
-    for j in range(MASTER_PER_MACHINE):
-        slave_port = port + 1000
+if not CLUSTER_NAME.endswith(str(cluster_id)):
+    print 'bad clusterid'
+    exit(1)
 
-        m = HOSTS[i]
-        s = HOSTS[(i+1)%len(HOSTS)]
-
-        #old format
-        #templete = "('$m:$port', '$BASEDIR/redis-$port'), ('$s:$slave_port', '$BASEDIR/redis-$slave_port'),"
-
-        #new format:
-        master_name = '%s-%s' % (CLUSTER_NAME, port)
-        templete = "'$master_name:$m:$port:$BASEDIR/redis-$port', '$master_name:$s:$slave_port:$BASEDIR/redis-$slave_port',"
-        print T(templete).s(globals())
+def gen_sentinel(port):
+    ret = ''
+    t_host = HOSTS[:3]
+    if len(t_host) < 3:  # host not enough
+        t_host = (t_host * 3) [:3]
+    for m in t_host:
+        templete = ' '*8 + "('$m:$port', '$BASEDIR/sentinel-$port'),\n"
+        ret += T(templete).s(dict(globals(), **locals()))
         port += 1
+    return ret
 
-# gen the "nutcracker" section
-port = 4000
-for i in range(len(HOSTS)):
-    m = HOSTS[i]
-    for j in range(MASTER_PER_MACHINE):
-        xport = port + j
-        templete = "('$m:$xport', '$BASEDIR/nutcracker-$xport'),"
-        print T(templete).s(globals())
+def gen_redis(port):
+    ret = ''
+    for i in range(len(HOSTS)):
+        for j in range(MASTER_PER_MACHINE):
+            slave_port = port + 1000
 
+            m = HOSTS[i]
+            s = HOSTS[(i+1)%len(HOSTS)]
+            master_name = '%s-%s' % (CLUSTER_NAME, port)
+            templete = ' '*8 + "'$master_name:$m:$port:$BASEDIR/redis-$port', '$master_name:$s:$slave_port:$BASEDIR/redis-$slave_port',\n"
+            ret += T(templete).s(dict(globals(), **locals()))
+            port += 1
+    return ret
+
+def gen_nutcracker(port):
+    ret = ''
+    for i in range(len(HOSTS)):
+        m = HOSTS[i]
+        for j in range(MASTER_PER_MACHINE):
+            xport = port + j
+            templete = ' '*8 + "('$m:$xport', '$BASEDIR/nutcracker-$xport'),\n"
+            ret += T(templete).s(dict(globals(), **locals()))
+    return ret
+
+def gen_cluster():
+    templete = '''
+$CLUSTER_NAME = {
+    'cluster_name': '$CLUSTER_NAME',
+    'user': '$USER',
+    'sentinel':[
+$sentinel_section
+    ],
+    'redis': [
+$redis_section
+    ],
+    'nutcracker': [
+$nutcracker_section
+    ],
+}
+    '''
+
+    redis_section      = gen_redis(2000+cluster_id*100)
+    nutcracker_section = gen_nutcracker(4000+cluster_id*100)
+    sentinel_section   = gen_sentinel(9000+cluster_id*100)
+
+    print T(templete).s(dict(globals(), **locals()))
+
+gen_cluster()
